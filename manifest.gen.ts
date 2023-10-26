4b170cc590c18245e418cfb725df5cf7d63bacd3
regiscuba2b <reginaldo.cuba@totvs.com.br>
2023-10-26 14:42:11 -0300

    feat: institucionaia

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -12,27 +12,28 @@ import * as $$$$$$6 from "./sections/Content/BannerSchool.tsx";
 import * as $$$$$$7 from "./sections/Content/BannerSlider.tsx";
 import * as $$$$$$8 from "./sections/Content/Benefits.tsx";
 import * as $$$$$$9 from "./sections/Content/Faq.tsx";
-import * as $$$$$$10 from "./sections/Content/Logos.tsx";
-import * as $$$$$$11 from "./sections/Content/Testimonials.tsx";
-import * as $$$$$$12 from "./sections/Content/TitleShelf.tsx";
-import * as $$$$$$13 from "./sections/Footer/Footer.tsx";
-import * as $$$$$$14 from "./sections/Header/Header.tsx";
-import * as $$$$$$15 from "./sections/Images/BannerGrid.tsx";
-import * as $$$$$$16 from "./sections/Images/Carousel.tsx";
-import * as $$$$$$17 from "./sections/Images/ImageGallery.tsx";
-import * as $$$$$$18 from "./sections/Images/ShoppableBanner.tsx";
-import * as $$$$$$19 from "./sections/Links/LinkTree.tsx";
-import * as $$$$$$20 from "./sections/Links/Shortcuts.tsx";
-import * as $$$$$$21 from "./sections/Miscellaneous/CampaignTimer.tsx";
-import * as $$$$$$22 from "./sections/Miscellaneous/CookieConsent.tsx";
-import * as $$$$$$23 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$24 from "./sections/Product/ProductDetails.tsx";
-import * as $$$$$$25 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$26 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$27 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$28 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$29 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$30 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$10 from "./sections/Content/Institucional.tsx";
+import * as $$$$$$11 from "./sections/Content/Logos.tsx";
+import * as $$$$$$12 from "./sections/Content/Testimonials.tsx";
+import * as $$$$$$13 from "./sections/Content/TitleShelf.tsx";
+import * as $$$$$$14 from "./sections/Footer/Footer.tsx";
+import * as $$$$$$15 from "./sections/Header/Header.tsx";
+import * as $$$$$$16 from "./sections/Images/BannerGrid.tsx";
+import * as $$$$$$17 from "./sections/Images/Carousel.tsx";
+import * as $$$$$$18 from "./sections/Images/ImageGallery.tsx";
+import * as $$$$$$19 from "./sections/Images/ShoppableBanner.tsx";
+import * as $$$$$$20 from "./sections/Links/LinkTree.tsx";
+import * as $$$$$$21 from "./sections/Links/Shortcuts.tsx";
+import * as $$$$$$22 from "./sections/Miscellaneous/CampaignTimer.tsx";
+import * as $$$$$$23 from "./sections/Miscellaneous/CookieConsent.tsx";
+import * as $$$$$$24 from "./sections/Newsletter/Newsletter.tsx";
+import * as $$$$$$25 from "./sections/Product/ProductDetails.tsx";
+import * as $$$$$$26 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$27 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$28 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$29 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$30 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$31 from "./sections/Theme/Theme.tsx";
 import * as $$$$$$$$$$$0 from "./apps/site.ts";
 
 const manifest = {
@@ -48,27 +49,28 @@ const manifest = {
     "deco-sites/maximus/sections/Content/BannerSlider.tsx": $$$$$$7,
     "deco-sites/maximus/sections/Content/Benefits.tsx": $$$$$$8,
     "deco-sites/maximus/sections/Content/Faq.tsx": $$$$$$9,
-    "deco-sites/maximus/sections/Content/Logos.tsx": $$$$$$10,
-    "deco-sites/maximus/sections/Content/Testimonials.tsx": $$$$$$11,
-    "deco-sites/maximus/sections/Content/TitleShelf.tsx": $$$$$$12,
-    "deco-sites/maximus/sections/Footer/Footer.tsx": $$$$$$13,
-    "deco-sites/maximus/sections/Header/Header.tsx": $$$$$$14,
-    "deco-sites/maximus/sections/Images/BannerGrid.tsx": $$$$$$15,
-    "deco-sites/maximus/sections/Images/Carousel.tsx": $$$$$$16,
-    "deco-sites/maximus/sections/Images/ImageGallery.tsx": $$$$$$17,
-    "deco-sites/maximus/sections/Images/ShoppableBanner.tsx": $$$$$$18,
-    "deco-sites/maximus/sections/Links/LinkTree.tsx": $$$$$$19,
-    "deco-sites/maximus/sections/Links/Shortcuts.tsx": $$$$$$20,
-    "deco-sites/maximus/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$21,
-    "deco-sites/maximus/sections/Miscellaneous/CookieConsent.tsx": $$$$$$22,
-    "deco-sites/maximus/sections/Newsletter/Newsletter.tsx": $$$$$$23,
-    "deco-sites/maximus/sections/Product/ProductDetails.tsx": $$$$$$24,
-    "deco-sites/maximus/sections/Product/ProductShelf.tsx": $$$$$$25,
-    "deco-sites/maximus/sections/Product/SearchResult.tsx": $$$$$$26,
-    "deco-sites/maximus/sections/Product/Wishlist.tsx": $$$$$$27,
-    "deco-sites/maximus/sections/Social/InstagramPosts.tsx": $$$$$$28,
-    "deco-sites/maximus/sections/Social/WhatsApp.tsx": $$$$$$29,
-    "deco-sites/maximus/sections/Theme/Theme.tsx": $$$$$$30,
+    "deco-sites/maximus/sections/Content/Institucional.tsx": $$$$$$10,
+    "deco-sites/maximus/sections/Content/Logos.tsx": $$$$$$11,
+    "deco-sites/maximus/sections/Content/Testimonials.tsx": $$$$$$12,
+    "deco-sites/maximus/sections/Content/TitleShelf.tsx": $$$$$$13,
+    "deco-sites/maximus/sections/Footer/Footer.tsx": $$$$$$14,
+    "deco-sites/maximus/sections/Header/Header.tsx": $$$$$$15,
+    "deco-sites/maximus/sections/Images/BannerGrid.tsx": $$$$$$16,
+    "deco-sites/maximus/sections/Images/Carousel.tsx": $$$$$$17,
+    "deco-sites/maximus/sections/Images/ImageGallery.tsx": $$$$$$18,
+    "deco-sites/maximus/sections/Images/ShoppableBanner.tsx": $$$$$$19,
+    "deco-sites/maximus/sections/Links/LinkTree.tsx": $$$$$$20,
+    "deco-sites/maximus/sections/Links/Shortcuts.tsx": $$$$$$21,
+    "deco-sites/maximus/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$22,
+    "deco-sites/maximus/sections/Miscellaneous/CookieConsent.tsx": $$$$$$23,
+    "deco-sites/maximus/sections/Newsletter/Newsletter.tsx": $$$$$$24,
+    "deco-sites/maximus/sections/Product/ProductDetails.tsx": $$$$$$25,
+    "deco-sites/maximus/sections/Product/ProductShelf.tsx": $$$$$$26,
+    "deco-sites/maximus/sections/Product/SearchResult.tsx": $$$$$$27,
+    "deco-sites/maximus/sections/Product/Wishlist.tsx": $$$$$$28,
+    "deco-sites/maximus/sections/Social/InstagramPosts.tsx": $$$$$$29,
+    "deco-sites/maximus/sections/Social/WhatsApp.tsx": $$$$$$30,
+    "deco-sites/maximus/sections/Theme/Theme.tsx": $$$$$$31,
   },
   "apps": {
     "deco-sites/maximus/apps/site.ts": $$$$$$$$$$$0,
---
+++ sections/Content/Institucional.tsx
@@ -0,0 +1,47 @@
+export interface Props {
+  title: string;
+  /** @format html */
+  html?: string;
+  /** @format textarea */
+  text?: string;
+}
+
+export default function Institucional(props: Props) {
+  const {
+    title = "Troca e Devolução",
+    html = "",
+    text = "Lorem...",
+  } = props;
+  return (
+    <div class="w-full max-w-[96%] md:max-w-[1236px] mx-auto mt-40 md:mt-20 py-[25px]">
+      <div class="flex items-center mb-5">
+        <span class="flex items-center text-xs tracking-[0px] text-neutral-800 capitalize underline after:content-[''] after:w-[3px] after:h-[3px] after:block after:bg-black after:mx-[7px] after:my-0 after:rounded-[50%]">
+          Home
+        </span>
+        <span class="text-xs font-normal leading-6 flex items-center">
+          {title}
+        </span>
+      </div>
+      <div class="border border-neutral-100 mb-[100px] px-5 md:px-10 py-[25px] border-solid">
+        <h1 class="text-[22px] font-semibold leading-[29px] tracking-[0px] text-[#171413] mb-10">
+          {title}
+        </h1>
+        {text && (
+          <p
+            class="text-[15px] text-shadow leading-[19px] tracking-[0px] text-neutral-800"
+            dangerouslySetInnerHTML={{ __html: text }}
+          >
+          </p>
+        )}
+        {html &&
+          (
+            <div
+              class="text-shadow leading-[19px] tracking-[0px] text-neutral-800"
+              dangerouslySetInnerHTML={{ __html: html }}
+            >
+            </div>
+          )}
+      </div>
+    </div>
+  );
+}
