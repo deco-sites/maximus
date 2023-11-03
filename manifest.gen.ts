57e34f54b0292cdd0fa0838de9e054670ba96ff6
regiscuba2b <reginaldo.cuba@totvs.com.br>
2023-11-03 18:44:30 -0300

    feat: institucional dicionario

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -16,28 +16,29 @@ import * as $$$$$$10 from "./sections/Content/Benefits.tsx";
 import * as $$$$$$11 from "./sections/Content/Faq.tsx";
 import * as $$$$$$12 from "./sections/Content/Institucional.tsx";
 import * as $$$$$$13 from "./sections/Content/InstitucionalDesenho.tsx";
-import * as $$$$$$14 from "./sections/Content/InstitucionalFaq.tsx";
-import * as $$$$$$15 from "./sections/Content/Logos.tsx";
-import * as $$$$$$16 from "./sections/Content/Testimonials.tsx";
-import * as $$$$$$17 from "./sections/Content/TitleShelf.tsx";
-import * as $$$$$$18 from "./sections/Footer/Footer.tsx";
-import * as $$$$$$19 from "./sections/Header/Header.tsx";
-import * as $$$$$$20 from "./sections/Images/BannerGrid.tsx";
-import * as $$$$$$21 from "./sections/Images/Carousel.tsx";
-import * as $$$$$$22 from "./sections/Images/ImageGallery.tsx";
-import * as $$$$$$23 from "./sections/Images/ShoppableBanner.tsx";
-import * as $$$$$$24 from "./sections/Links/LinkTree.tsx";
-import * as $$$$$$25 from "./sections/Links/Shortcuts.tsx";
-import * as $$$$$$26 from "./sections/Miscellaneous/CampaignTimer.tsx";
-import * as $$$$$$27 from "./sections/Miscellaneous/CookieConsent.tsx";
-import * as $$$$$$28 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$29 from "./sections/Product/ProductDetails.tsx";
-import * as $$$$$$30 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$31 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$32 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$33 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$34 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$35 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$14 from "./sections/Content/InstitucionalDicionario.tsx";
+import * as $$$$$$15 from "./sections/Content/InstitucionalFaq.tsx";
+import * as $$$$$$16 from "./sections/Content/Logos.tsx";
+import * as $$$$$$17 from "./sections/Content/Testimonials.tsx";
+import * as $$$$$$18 from "./sections/Content/TitleShelf.tsx";
+import * as $$$$$$19 from "./sections/Footer/Footer.tsx";
+import * as $$$$$$20 from "./sections/Header/Header.tsx";
+import * as $$$$$$21 from "./sections/Images/BannerGrid.tsx";
+import * as $$$$$$22 from "./sections/Images/Carousel.tsx";
+import * as $$$$$$23 from "./sections/Images/ImageGallery.tsx";
+import * as $$$$$$24 from "./sections/Images/ShoppableBanner.tsx";
+import * as $$$$$$25 from "./sections/Links/LinkTree.tsx";
+import * as $$$$$$26 from "./sections/Links/Shortcuts.tsx";
+import * as $$$$$$27 from "./sections/Miscellaneous/CampaignTimer.tsx";
+import * as $$$$$$28 from "./sections/Miscellaneous/CookieConsent.tsx";
+import * as $$$$$$29 from "./sections/Newsletter/Newsletter.tsx";
+import * as $$$$$$30 from "./sections/Product/ProductDetails.tsx";
+import * as $$$$$$31 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$32 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$33 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$34 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$35 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$36 from "./sections/Theme/Theme.tsx";
 import * as $$$$$$$$$$$0 from "./apps/site.ts";
 
 const manifest = {
@@ -57,28 +58,29 @@ const manifest = {
     "deco-sites/maximus/sections/Content/Faq.tsx": $$$$$$11,
     "deco-sites/maximus/sections/Content/Institucional.tsx": $$$$$$12,
     "deco-sites/maximus/sections/Content/InstitucionalDesenho.tsx": $$$$$$13,
-    "deco-sites/maximus/sections/Content/InstitucionalFaq.tsx": $$$$$$14,
-    "deco-sites/maximus/sections/Content/Logos.tsx": $$$$$$15,
-    "deco-sites/maximus/sections/Content/Testimonials.tsx": $$$$$$16,
-    "deco-sites/maximus/sections/Content/TitleShelf.tsx": $$$$$$17,
-    "deco-sites/maximus/sections/Footer/Footer.tsx": $$$$$$18,
-    "deco-sites/maximus/sections/Header/Header.tsx": $$$$$$19,
-    "deco-sites/maximus/sections/Images/BannerGrid.tsx": $$$$$$20,
-    "deco-sites/maximus/sections/Images/Carousel.tsx": $$$$$$21,
-    "deco-sites/maximus/sections/Images/ImageGallery.tsx": $$$$$$22,
-    "deco-sites/maximus/sections/Images/ShoppableBanner.tsx": $$$$$$23,
-    "deco-sites/maximus/sections/Links/LinkTree.tsx": $$$$$$24,
-    "deco-sites/maximus/sections/Links/Shortcuts.tsx": $$$$$$25,
-    "deco-sites/maximus/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$26,
-    "deco-sites/maximus/sections/Miscellaneous/CookieConsent.tsx": $$$$$$27,
-    "deco-sites/maximus/sections/Newsletter/Newsletter.tsx": $$$$$$28,
-    "deco-sites/maximus/sections/Product/ProductDetails.tsx": $$$$$$29,
-    "deco-sites/maximus/sections/Product/ProductShelf.tsx": $$$$$$30,
-    "deco-sites/maximus/sections/Product/SearchResult.tsx": $$$$$$31,
-    "deco-sites/maximus/sections/Product/Wishlist.tsx": $$$$$$32,
-    "deco-sites/maximus/sections/Social/InstagramPosts.tsx": $$$$$$33,
-    "deco-sites/maximus/sections/Social/WhatsApp.tsx": $$$$$$34,
-    "deco-sites/maximus/sections/Theme/Theme.tsx": $$$$$$35,
+    "deco-sites/maximus/sections/Content/InstitucionalDicionario.tsx": $$$$$$14,
+    "deco-sites/maximus/sections/Content/InstitucionalFaq.tsx": $$$$$$15,
+    "deco-sites/maximus/sections/Content/Logos.tsx": $$$$$$16,
+    "deco-sites/maximus/sections/Content/Testimonials.tsx": $$$$$$17,
+    "deco-sites/maximus/sections/Content/TitleShelf.tsx": $$$$$$18,
+    "deco-sites/maximus/sections/Footer/Footer.tsx": $$$$$$19,
+    "deco-sites/maximus/sections/Header/Header.tsx": $$$$$$20,
+    "deco-sites/maximus/sections/Images/BannerGrid.tsx": $$$$$$21,
+    "deco-sites/maximus/sections/Images/Carousel.tsx": $$$$$$22,
+    "deco-sites/maximus/sections/Images/ImageGallery.tsx": $$$$$$23,
+    "deco-sites/maximus/sections/Images/ShoppableBanner.tsx": $$$$$$24,
+    "deco-sites/maximus/sections/Links/LinkTree.tsx": $$$$$$25,
+    "deco-sites/maximus/sections/Links/Shortcuts.tsx": $$$$$$26,
+    "deco-sites/maximus/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$27,
+    "deco-sites/maximus/sections/Miscellaneous/CookieConsent.tsx": $$$$$$28,
+    "deco-sites/maximus/sections/Newsletter/Newsletter.tsx": $$$$$$29,
+    "deco-sites/maximus/sections/Product/ProductDetails.tsx": $$$$$$30,
+    "deco-sites/maximus/sections/Product/ProductShelf.tsx": $$$$$$31,
+    "deco-sites/maximus/sections/Product/SearchResult.tsx": $$$$$$32,
+    "deco-sites/maximus/sections/Product/Wishlist.tsx": $$$$$$33,
+    "deco-sites/maximus/sections/Social/InstagramPosts.tsx": $$$$$$34,
+    "deco-sites/maximus/sections/Social/WhatsApp.tsx": $$$$$$35,
+    "deco-sites/maximus/sections/Theme/Theme.tsx": $$$$$$36,
   },
   "apps": {
     "deco-sites/maximus/apps/site.ts": $$$$$$$$$$$0,
---
+++ sections/Content/InstitucionalDicionario.tsx
@@ -0,0 +1,110 @@
+export interface Props {
+  title: string;
+}
+
+export default function InstitucionalDicionario(props: Props) {
+  const {
+    title = "Troca e Devolução",
+  } = props;
+  return (
+    <div class="w-full mt-32 md:mt-10 mb-[-32px] border-b border-[#ebebeb]">
+      <div class="max-w-[94%] md:max-w-[1246px] mx-auto">
+        <div class="flex items-center mb-5">
+          <span class="flex items-center text-xs tracking-[0px] text-neutral-800 capitalize underline after:content-[''] after:w-[3px] after:h-[3px] after:block after:bg-black after:mx-[7px] after:my-0 after:rounded-[50%]">
+            Home
+          </span>
+          <span class="text-xs font-normal leading-6 flex items-center">
+            {title}
+          </span>
+        </div>
+
+        <h1 class="font-semibold leading-[29px] text-2xl text-neutral-800 mb-4 mx-0 my-[0.67em] max-md:text-lg max-md:mt-10">
+          Dicionário de Tecidos
+        </h1>
+        <p class="text-[15px] w-full md:w-[85%] tracking-[0] text-neutral-800 leading-[21px] mt-5 mb-[50px]">
+          Conhecer a origem e as principais características dos tecidos é
+          fundamental para fazer a escolha certa. Por isso, criamos um
+          Dicionário de Tecidos repleto de informações que vão te auxiliar na
+          hora da compra. Antes de começar, é preciso esclarecer alguns
+          conceitos básicos que vão facilitar o entendimento do que vem adiante.
+          Vamos lá?!
+        </p>
+
+        <div class="flex max-md:flex-col justify-between">
+          <div class="w-full md:w-[45%] max-md:mb-10">
+            <h3 class="text-[15px] text-neutral-800 uppercase font-medium mb-[15px] text-shadow">
+              TECIDOS EM FIBRAS NATURAIS:
+            </h3>
+            <p class="text-[13px] tracking-[0] text-neutral-800 leading-[21px]">
+              São tecidos feitos a partir de matérias-primas presentes na
+              natureza. Possuem um valor agregado bem superior aos sintéticos,
+              mas demandam também muito mais cuidado. Estes tecidos não absorvem
+              o calor, são bem fresquinhos, mas não podem, em hipótese alguma,
+              ser mergulhados em água. A lavagem precisa ser à seco.
+              <br />
+              <br />
+              Lavar um tecido com fibra natural é condená-lo à morte. Alguns
+              ficam com algumas pontas mais curtas e outras mais longas, outros
+              encolhem de 1 a 2 números e outros ficam completamente deformados.
+              Então, não esqueça: mantenha suas peças confeccionadas com fibras
+              naturais bem longe da máquina de lavar!
+            </p>
+          </div>
+          <div class="w-full md:w-[45%]">
+            <h3 class="text-[15px] text-neutral-800 uppercase font-medium mb-[15px] text-shadow">
+              TECIDOS EM FIBRAS SINTÉTICAS:
+            </h3>
+            <p class="text-[13px] tracking-[0] text-neutral-800 leading-[21px]">
+              São aqueles tecidos cujas fibras são produzidas a partir de
+              produtos químicos como o poliéster. São mais duráveis e fáceis de
+              cuidar, podendo ser lavados sem problemas, porém são menos
+              sofisticados do que as fibras naturais.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="w-full mt-7 md:mt-10 px-0 py-[50px] bg-[#f5f5f5]">
+        <div class="max-w-[94%] md:max-w-[1246px] mx-auto">
+          <details class="w-full collapse collapse-arrow join-itemborder mb-[25px] px-2 md:px-10 rounded-sm border border-[#EBEBEB] bg-white">
+            <summary class="collapse-title text-lg font-medium">
+              <div class="flex items-center">
+                <img
+                  width={45}
+                  height={45}
+                  src="https://tfcszo.vteximg.com.br/arquivos/inst-image-tecido.png"
+                  alt="thumb do tecido"
+                />
+                <span class="text-neutral-800 text-[15px] uppercase font-semibold mx-10 my-0">
+                  ALFAIATARIA
+                </span>{" "}
+                <a href="#" class="text-xs text-neutral-800 underline">
+                  Ver na loja
+                </a>
+              </div>
+            </summary>
+            <div class="collapse-content text-[13px] tracking-[0] text-neutral-800 leading-[21px] mb-[18px]">
+              Os tecidos de alfaiataria nunca saem de moda, pois são elegantes e
+              vestem bem todos os estilos. São mais estruturados, perfeitos para
+              confecção de peças clássicas como ternos e blazers, tanto
+              masculinos quanto femininos. Os tecidos sintéticos amassam menos e
+              possuem excelente custo benefício.
+              <br />
+              <br />
+              <strong>Sugestões de modelos com tecidos de alfaiataria:</strong>
+              <br />
+              <br />
+
+              - Moda festa: clássicos da alfaiataria como blazer, tubinhos,
+              macacão, além de vestidos e saias;
+
+              - Moda casual: vestidos e saias mais justas ou com volume e
+              blazers.
+            </div>
+          </details>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
