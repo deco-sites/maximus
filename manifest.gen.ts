53a29fb5e4d88d3c10695dd4a30c09378769fa54
regiscuba2b <reginaldo.cuba@totvs.com.br>
2023-11-10 18:52:33 -0300

    feat: quem somos

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -15,34 +15,35 @@ import * as $$$$$$9 from "./sections/Content/BannerSlider2.tsx";
 import * as $$$$$$10 from "./sections/Content/Benefits.tsx";
 import * as $$$$$$11 from "./sections/Content/Faq.tsx";
 import * as $$$$$$12 from "./sections/Content/Institucional.tsx";
-import * as $$$$$$13 from "./sections/Content/InstitucionalClube.tsx";
-import * as $$$$$$14 from "./sections/Content/InstitucionalDesenho.tsx";
-import * as $$$$$$15 from "./sections/Content/InstitucionalDicionario.tsx";
-import * as $$$$$$16 from "./sections/Content/InstitucionalEstudante.tsx";
-import * as $$$$$$17 from "./sections/Content/InstitucionalFaq.tsx";
-import * as $$$$$$18 from "./sections/Content/InstitucionalLojas.tsx";
-import * as $$$$$$19 from "./sections/Content/Logos.tsx";
-import * as $$$$$$20 from "./sections/Content/LojasItem.tsx";
-import * as $$$$$$21 from "./sections/Content/Testimonials.tsx";
-import * as $$$$$$22 from "./sections/Content/TitleShelf.tsx";
-import * as $$$$$$23 from "./sections/Footer/Footer.tsx";
-import * as $$$$$$24 from "./sections/Header/Header.tsx";
-import * as $$$$$$25 from "./sections/Images/BannerGrid.tsx";
-import * as $$$$$$26 from "./sections/Images/Carousel.tsx";
-import * as $$$$$$27 from "./sections/Images/ImageGallery.tsx";
-import * as $$$$$$28 from "./sections/Images/ShoppableBanner.tsx";
-import * as $$$$$$29 from "./sections/Links/LinkTree.tsx";
-import * as $$$$$$30 from "./sections/Links/Shortcuts.tsx";
-import * as $$$$$$31 from "./sections/Miscellaneous/CampaignTimer.tsx";
-import * as $$$$$$32 from "./sections/Miscellaneous/CookieConsent.tsx";
-import * as $$$$$$33 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$34 from "./sections/Product/ProductDetails.tsx";
-import * as $$$$$$35 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$36 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$37 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$38 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$39 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$40 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$13 from "./sections/Content/InstitucionalAbout.tsx";
+import * as $$$$$$14 from "./sections/Content/InstitucionalClube.tsx";
+import * as $$$$$$15 from "./sections/Content/InstitucionalDesenho.tsx";
+import * as $$$$$$16 from "./sections/Content/InstitucionalDicionario.tsx";
+import * as $$$$$$17 from "./sections/Content/InstitucionalEstudante.tsx";
+import * as $$$$$$18 from "./sections/Content/InstitucionalFaq.tsx";
+import * as $$$$$$19 from "./sections/Content/InstitucionalLojas.tsx";
+import * as $$$$$$20 from "./sections/Content/Logos.tsx";
+import * as $$$$$$21 from "./sections/Content/LojasItem.tsx";
+import * as $$$$$$22 from "./sections/Content/Testimonials.tsx";
+import * as $$$$$$23 from "./sections/Content/TitleShelf.tsx";
+import * as $$$$$$24 from "./sections/Footer/Footer.tsx";
+import * as $$$$$$25 from "./sections/Header/Header.tsx";
+import * as $$$$$$26 from "./sections/Images/BannerGrid.tsx";
+import * as $$$$$$27 from "./sections/Images/Carousel.tsx";
+import * as $$$$$$28 from "./sections/Images/ImageGallery.tsx";
+import * as $$$$$$29 from "./sections/Images/ShoppableBanner.tsx";
+import * as $$$$$$30 from "./sections/Links/LinkTree.tsx";
+import * as $$$$$$31 from "./sections/Links/Shortcuts.tsx";
+import * as $$$$$$32 from "./sections/Miscellaneous/CampaignTimer.tsx";
+import * as $$$$$$33 from "./sections/Miscellaneous/CookieConsent.tsx";
+import * as $$$$$$34 from "./sections/Newsletter/Newsletter.tsx";
+import * as $$$$$$35 from "./sections/Product/ProductDetails.tsx";
+import * as $$$$$$36 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$37 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$38 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$39 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$40 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$41 from "./sections/Theme/Theme.tsx";
 import * as $$$$$$$$$$$0 from "./apps/site.ts";
 
 const manifest = {
@@ -61,34 +62,35 @@ const manifest = {
     "deco-sites/maximus/sections/Content/Benefits.tsx": $$$$$$10,
     "deco-sites/maximus/sections/Content/Faq.tsx": $$$$$$11,
     "deco-sites/maximus/sections/Content/Institucional.tsx": $$$$$$12,
-    "deco-sites/maximus/sections/Content/InstitucionalClube.tsx": $$$$$$13,
-    "deco-sites/maximus/sections/Content/InstitucionalDesenho.tsx": $$$$$$14,
-    "deco-sites/maximus/sections/Content/InstitucionalDicionario.tsx": $$$$$$15,
-    "deco-sites/maximus/sections/Content/InstitucionalEstudante.tsx": $$$$$$16,
-    "deco-sites/maximus/sections/Content/InstitucionalFaq.tsx": $$$$$$17,
-    "deco-sites/maximus/sections/Content/InstitucionalLojas.tsx": $$$$$$18,
-    "deco-sites/maximus/sections/Content/Logos.tsx": $$$$$$19,
-    "deco-sites/maximus/sections/Content/LojasItem.tsx": $$$$$$20,
-    "deco-sites/maximus/sections/Content/Testimonials.tsx": $$$$$$21,
-    "deco-sites/maximus/sections/Content/TitleShelf.tsx": $$$$$$22,
-    "deco-sites/maximus/sections/Footer/Footer.tsx": $$$$$$23,
-    "deco-sites/maximus/sections/Header/Header.tsx": $$$$$$24,
-    "deco-sites/maximus/sections/Images/BannerGrid.tsx": $$$$$$25,
-    "deco-sites/maximus/sections/Images/Carousel.tsx": $$$$$$26,
-    "deco-sites/maximus/sections/Images/ImageGallery.tsx": $$$$$$27,
-    "deco-sites/maximus/sections/Images/ShoppableBanner.tsx": $$$$$$28,
-    "deco-sites/maximus/sections/Links/LinkTree.tsx": $$$$$$29,
-    "deco-sites/maximus/sections/Links/Shortcuts.tsx": $$$$$$30,
-    "deco-sites/maximus/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$31,
-    "deco-sites/maximus/sections/Miscellaneous/CookieConsent.tsx": $$$$$$32,
-    "deco-sites/maximus/sections/Newsletter/Newsletter.tsx": $$$$$$33,
-    "deco-sites/maximus/sections/Product/ProductDetails.tsx": $$$$$$34,
-    "deco-sites/maximus/sections/Product/ProductShelf.tsx": $$$$$$35,
-    "deco-sites/maximus/sections/Product/SearchResult.tsx": $$$$$$36,
-    "deco-sites/maximus/sections/Product/Wishlist.tsx": $$$$$$37,
-    "deco-sites/maximus/sections/Social/InstagramPosts.tsx": $$$$$$38,
-    "deco-sites/maximus/sections/Social/WhatsApp.tsx": $$$$$$39,
-    "deco-sites/maximus/sections/Theme/Theme.tsx": $$$$$$40,
+    "deco-sites/maximus/sections/Content/InstitucionalAbout.tsx": $$$$$$13,
+    "deco-sites/maximus/sections/Content/InstitucionalClube.tsx": $$$$$$14,
+    "deco-sites/maximus/sections/Content/InstitucionalDesenho.tsx": $$$$$$15,
+    "deco-sites/maximus/sections/Content/InstitucionalDicionario.tsx": $$$$$$16,
+    "deco-sites/maximus/sections/Content/InstitucionalEstudante.tsx": $$$$$$17,
+    "deco-sites/maximus/sections/Content/InstitucionalFaq.tsx": $$$$$$18,
+    "deco-sites/maximus/sections/Content/InstitucionalLojas.tsx": $$$$$$19,
+    "deco-sites/maximus/sections/Content/Logos.tsx": $$$$$$20,
+    "deco-sites/maximus/sections/Content/LojasItem.tsx": $$$$$$21,
+    "deco-sites/maximus/sections/Content/Testimonials.tsx": $$$$$$22,
+    "deco-sites/maximus/sections/Content/TitleShelf.tsx": $$$$$$23,
+    "deco-sites/maximus/sections/Footer/Footer.tsx": $$$$$$24,
+    "deco-sites/maximus/sections/Header/Header.tsx": $$$$$$25,
+    "deco-sites/maximus/sections/Images/BannerGrid.tsx": $$$$$$26,
+    "deco-sites/maximus/sections/Images/Carousel.tsx": $$$$$$27,
+    "deco-sites/maximus/sections/Images/ImageGallery.tsx": $$$$$$28,
+    "deco-sites/maximus/sections/Images/ShoppableBanner.tsx": $$$$$$29,
+    "deco-sites/maximus/sections/Links/LinkTree.tsx": $$$$$$30,
+    "deco-sites/maximus/sections/Links/Shortcuts.tsx": $$$$$$31,
+    "deco-sites/maximus/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$32,
+    "deco-sites/maximus/sections/Miscellaneous/CookieConsent.tsx": $$$$$$33,
+    "deco-sites/maximus/sections/Newsletter/Newsletter.tsx": $$$$$$34,
+    "deco-sites/maximus/sections/Product/ProductDetails.tsx": $$$$$$35,
+    "deco-sites/maximus/sections/Product/ProductShelf.tsx": $$$$$$36,
+    "deco-sites/maximus/sections/Product/SearchResult.tsx": $$$$$$37,
+    "deco-sites/maximus/sections/Product/Wishlist.tsx": $$$$$$38,
+    "deco-sites/maximus/sections/Social/InstagramPosts.tsx": $$$$$$39,
+    "deco-sites/maximus/sections/Social/WhatsApp.tsx": $$$$$$40,
+    "deco-sites/maximus/sections/Theme/Theme.tsx": $$$$$$41,
   },
   "apps": {
     "deco-sites/maximus/apps/site.ts": $$$$$$$$$$$0,
---
+++ sections/Content/InstitucionalAbout.tsx
@@ -0,0 +1,350 @@
+export interface Props {
+  breadcrumb?: string;
+  title?: string;
+  /** @format html */
+  html?: string;
+  /** @format textarea */
+  text?: string;
+}
+
+export default function InstitucionalAbout(props: Props) {
+  const {
+    breadcrumb = "",
+    title = "sobre-nos",
+    html = "",
+    text = "Lorem...",
+  } = props;
+  return (
+    <div class="w-full mt-20 md:mt-0 pt-[15px] mb-[-32px] border-b-[#ebebeb] border-b border-solid">
+      {/* banner main */}
+      <div class="w-full max-w-[1236px] mx-auto flex items-center max-md:flex-col relative before:content-[''] before:w-full before:h-[716px] before:absolute before:-left-full before:bg-[#22201f]">
+        <div class="w-full md:w-[875px] md:h-[716px] bg-[#22201F]">
+          <div class="w-full md:w-[508px] block pt-[50px] md:pt-[70px] max-md:pb-[100px] pb-0 px-[20px] md:px-[50px]">
+            <img
+              width={45}
+              height={45}
+              src="https://tfcszo.vteximg.com.br/arquivos/logo-sobre-nos.png"
+              alt="logo"
+            />
+            <h1 class="text-[28px] leading-[34px] md:text-[38px] font-normal md:leading-[47px] text-white my-4">
+              Resgatar o hábito de costurar
+            </h1>
+            <h3 class="text-[22px] uppercase font-light tracking-[0.50px] leading-[22px] text-white mt-[13px] mb-[50px] mx-0">
+              ESSE É O NOSSO PROPÓSITO.
+            </h3>
+            <p class="text-[13px] tracking-[0] leading-[21px] text-white mb-[18px]">
+              A Maximus Tecidos nasceu de uma missão muito especial: resgatar um
+              hábito que sempre esteve muito presente na vida das pessoas e com
+              o tempo acabou se perdendo.
+              <br />
+              <br />
+              Provavelmente na sua família alguém já teve a costura como um
+              costume, mas hoje está esquecido e as velhas máquinas que
+              antigamente eram o braço direito da família, ficaram de lado em
+              algum cômodo da casa.
+              <br />
+              <br />
+              A costura, muito mais do que trabalho, é uma forma de lazer e
+              inspiração. Através dela é possível transformar o mundo, ou ao
+              menos a visão sobre ele. Trazer um porquê para as nossas atitudes
+              e consciência sobre nossas escolhas. É uma causa que nós
+              abraçamos, pois acreditamos que as novas gerações merecem conhecer
+              esses valores e tradições que carregam muitas histórias de valor.
+            </p>
+          </div>
+        </div>
+        <img
+          class="md:absolute md:right-0 max-md:mt-[-70px] max-md:w-[80%]"
+          width={510}
+          height={551}
+          src="https://tfcszo.vteximg.com.br/arquivos/header-sobre-nos.png"
+          alt="quem somos"
+        />
+      </div>
+
+      {/* first section */}
+      <div class="max-w-[1236px] mx-auto my-[60px] md:my-[120px] flex max-md:flex-col items-center justify-between max-md:px-[20px]">
+        <img
+          class="max-w-full max-md:hidden"
+          width={540}
+          height={412}
+          src="https://tfcszo.vteximg.com.br/arquivos/sobre-nos-escola-online.png"
+          alt="banner"
+        />
+        <div class="max-w-full w-[508px]">
+          <h3 class="text-[15px] text-neutral-800 uppercase font-medium text-shadow mb-[22px]">
+            ESCOLA DE MODA ONLINE
+          </h3>
+          <p class="text-[13px] tracking-[0] text-neutral-800 leading-[21px] mb-[18px]">
+            A Maximus Tecidos acredita no poder da costura como um movimento
+            capaz de transformar vidas e expandiu suas atividades no ramo da
+            educação. Além dos materiais educacionais que se tornaram um dos
+            carros chefes, agora também conta com a Maximus Escola de Moda
+            Online, que já conta com mais de 37.000 alunas (os), com cursos de
+            Desenho de Moda, Corte e Costura (reconhecido pelo MEC), Costura
+            Pet, Costura Infantil, Costura Criativa, Modelagem (reconhecido pelo
+            MEC), Bordados, Marketing e Negócios para Costura.
+            <br />
+            <br />
+            A missão é lançar outros cursos no segmento da Moda e se tornar uma
+            das maiores Escolas de Moda Online do Brasil e redescobrir a arte de
+            fazer moda com as próprias mãos.
+          </p>
+          <a
+            class="w-full md:w-[200px] text-white border text-[13px] font-medium text-center block mt-[41px] px-0 py-3.5 rounded-[3px] border-solid border-[#171413] bg-[#171413] hover:text-[#171413] hover:border-[#171413] hover:bg-white transition-all"
+            href="https://materiais.maximustecidos.com.br/maximus-escola-de-moda-online?_gl=1*vttyui*_ga*MzgyMjYzODQ2LjE2OTg3MDc2MzM.*_ga_HGM4SXF2FQ*MTY5OTYzMjA3Ny4yMi4wLjE2OTk2MzIwOTkuMC4wLjA.*_fplc*RWpuMDRWV1BrTjk2cW1tb1hQeDRjV2x5VFpYQW0ydE4lMkJ0M0wlMkJLOUMlMkJnNW5jWlh0d3JtaFY1N3NxT3J1eDFzaHJnUm9HdVFmN2QwMSUyRjhmbWlJaTAydmpSJTJGc1BtaU1KdGFxSlVNeSUyRnNSTjkzcWVqJTJGMXVuY2E3JTJCNnRHajN1QSUzRCUzRA.."
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Conheça nossos cursos
+          </a>
+        </div>
+      </div>
+
+      {/* last section */}
+      <div class="w-full bg-[#f5f5f5] pt-[50px] md:pt-[100px] pb-[75px] px-0">
+        <div class="w-[1190px] max-w-full mx-auto my-0 px-5">
+          <h3 class="text-[29px] font-light uppercase text-center text-neutral-800">
+            LINHA DO TEMPO
+          </h3>
+
+          {/* time mobile */}
+          <div class="w-full flex md:hidden items-center flex-col mt-5">
+            <div class="flex flex-col items-center justify-center after:pontinhos mb-3 w-full">
+              <h5 class="text-[#E4A886] text-2xl text-center font-medium mb-[5px]">
+                1986
+              </h5>
+              <p class="font-normal leading-6 text-center text-[13px] mb-3">
+                Nasce o grupo Maximus com a costureira e estilista Inês Maximus,
+                abrindo um comércio de confecção chamado inicialmente de
+                Alfaiataria modelo
+              </p>
+            </div>
+
+            <div class="flex flex-col items-center justify-center after:pontinhos mb-3 w-full">
+              <h5 class="text-[#E4A886] text-2xl text-center font-medium mb-[5px]">
+                2013
+              </h5>
+              <p class="font-normal leading-6 text-center text-[13px] mb-3">
+                Abertura da primeira loja de Tecidos
+              </p>
+            </div>
+
+            <div class="flex flex-col items-center justify-center after:pontinhos mb-3 w-full">
+              <h5 class="text-[#E4A886] text-2xl text-center font-medium mb-[5px]">
+                2015
+              </h5>
+              <p class="font-normal leading-6 text-center text-[13px] mb-3">
+                Início da Loja Virtual e do Blog da Costureira
+              </p>
+            </div>
+
+            <div class="flex flex-col items-center justify-center after:pontinhos mb-3 w-full">
+              <h5 class="text-[#E4A886] text-2xl text-center font-medium mb-[5px]">
+                2017
+              </h5>
+              <p class="font-normal leading-6 text-center text-[13px] mb-3">
+                Lançamento da Rádio da Costureira
+              </p>
+            </div>
+
+            <div class="flex flex-col items-center justify-center after:pontinhos mb-3 w-full">
+              <h5 class="text-[#E4A886] text-2xl text-center font-medium mb-[5px]">
+                2018
+              </h5>
+              <p class="font-normal leading-6 text-center text-[13px] mb-3">
+                Primeiro curso (presencial) em parceria com o Estilista Valmir
+                Pazeto
+              </p>
+            </div>
+
+            <div class="flex flex-col items-center justify-center after:pontinhos mb-3 w-full">
+              <h5 class="text-[#E4A886] text-2xl text-center font-medium mb-[5px]">
+                2019
+              </h5>
+              <p class="font-normal leading-6 text-center text-[13px] mb-3">
+                Primeiro curso Online de Costura com Nea Santtana
+              </p>
+            </div>
+
+            <div class="flex flex-col items-center justify-center after:pontinhos mb-3 w-full">
+              <h5 class="text-[#E4A886] text-2xl text-center font-medium mb-[5px]">
+                2020
+              </h5>
+              <p class="font-normal leading-6 text-center text-[13px] mb-3">
+                Lançamento Oficial da Escola de Moda On-line com as Experts
+                Marlene Mukai, Aury Mesquita e outros profissionais renomados.
+              </p>
+            </div>
+
+            <div class="flex flex-col items-center justify-center w-full mb-6">
+              <h5 class="text-[#E4A886] text-2xl text-center font-medium mb-[5px]">
+                2022
+              </h5>
+              <p class="font-normal leading-6 text-center text-[13px] mb-3">
+              Lançamento do Marketplace. Espalhar o nosso propósito e formar
+                milhares de alunos no mundo todo
+              </p>
+            </div>            
+          </div>
+
+          {/* time desktop */}
+          <div class="hidden md:flex items-center justify-between mt-[50px] mb-[120px] mx-0">
+            <div class="w-[970px]">
+              <div class="w-full flex justify-between items-center relative after:pontinhos-base">
+                <div class="flex flex-col items-center justify-center after:pontinhos w-full md:w-[280px] ">
+                  <h5 class="text-[#E4A886] text-2xl text-center font-medium mb-[5px]">
+                    1986
+                  </h5>
+                  <p class="min-h-[96px] font-normal leading-6 text-center text-[13px] my-3">
+                    Nasce o grupo Maximus com a costureira e estilista Inês
+                    Maximus, abrindo um comércio de confecção chamado
+                    inicialmente de Alfaiataria modelo
+                  </p>
+                  <img
+                    class="mb-3"
+                    width={33}
+                    height={23}
+                    src="https://tfcszo.vteximg.com.br/arquivos/logo-linha-do-tem.png"
+                    alt="logo"
+                  />
+                </div>
+
+                <div class="flex flex-col items-center justify-center after:pontinhos w-full md:w-[120px] ">
+                  <h5 class="text-[#E4A886] text-2xl text-center font-medium mb-[5px]">
+                    2015
+                  </h5>
+                  <p class="min-h-[96px] font-normal leading-6 text-center text-[13px] my-3">
+                    Início da Loja Virtual e do Blog da Costureira
+                  </p>
+                  <img
+                    class="mb-3"
+                    width={33}
+                    height={23}
+                    src="https://tfcszo.vteximg.com.br/arquivos/logo-linha-do-tem.png"
+                    alt="logo"
+                  />
+                </div>
+
+                <div class="flex flex-col items-center justify-center after:pontinhos w-full md:w-[180px] ">
+                  <h5 class="text-[#E4A886] text-2xl text-center font-medium mb-[5px]">
+                    2018
+                  </h5>
+                  <p class="min-h-[96px] font-normal leading-6 text-center text-[13px] my-3">
+                    Primeiro curso (presencial) em parceria com o Estilista
+                    Valmir Pazeto
+                  </p>
+                  <img
+                    class="mb-3"
+                    width={33}
+                    height={23}
+                    src="https://tfcszo.vteximg.com.br/arquivos/logo-linha-do-tem.png"
+                    alt="logo"
+                  />
+                </div>
+
+                <div class="flex flex-col items-center justify-center after:pontinhos w-full md:w-[217px] ">
+                  <h5 class="text-[#E4A886] text-2xl text-center font-medium mb-[5px]">
+                    2020
+                  </h5>
+                  <p class="min-h-[96px] font-normal leading-6 text-center text-[13px] my-3">
+                    Lançamento Oficial da Escola de Moda On-line com as Experts
+                    Marlene Mukai, Aury Mesquita e outros profissionais
+                    renomados.
+                  </p>
+                  <img
+                    class="mb-3"
+                    width={33}
+                    height={23}
+                    src="https://tfcszo.vteximg.com.br/arquivos/logo-linha-do-tem.png"
+                    alt="logo"
+                  />
+                </div>
+              </div>
+
+              <div class="w-full flex justify-center items-center">
+                <div class="flex flex-col items-center justify-center before:pontinhos w-full md:w-[141px] mr-[70px]">
+                  <img
+                    class="mb-3"
+                    width={33}
+                    height={23}
+                    src="https://tfcszo.vteximg.com.br/arquivos/logo-linha-do-tem.png"
+                    alt="logo"
+                  />
+                  <h5 class="text-[#E4A886] text-2xl text-center font-medium mb-[5px]">
+                    2013
+                  </h5>
+                  <p class="font-normal leading-6 text-center text-[13px] my-3">
+                    Abertura da primeira loja de Tecidos
+                  </p>
+                </div>
+
+                <div class="flex flex-col items-center justify-center before:pontinhos w-full md:w-[141px] mr-[70px]">
+                  <img
+                    class="mb-3"
+                    width={33}
+                    height={23}
+                    src="https://tfcszo.vteximg.com.br/arquivos/logo-linha-do-tem.png"
+                    alt="logo"
+                  />
+                  <h5 class="text-[#E4A886] text-2xl text-center font-medium mb-[5px]">
+                    2017
+                  </h5>
+                  <p class="font-normal leading-6 text-center text-[13px] my-3">
+                    Lançamento da Rádio da Costureira
+                  </p>
+                </div>
+
+                <div class="flex flex-col items-center justify-center before:pontinhos w-full md:w-[180px]">
+                  <img
+                    class="mb-3"
+                    width={33}
+                    height={23}
+                    src="https://tfcszo.vteximg.com.br/arquivos/logo-linha-do-tem.png"
+                    alt="logo"
+                  />
+                  <h5 class="text-[#E4A886] text-2xl text-center font-medium mb-[5px]">
+                    2019
+                  </h5>
+                  <p class="font-normal leading-6 text-center text-[13px] my-3">
+                    Primeiro curso Online de Costura com Nea Santtana
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="w-[190px] flex items-center flex-col">
+              <div class="flex items-center gap-4 mb-2">
+                <img
+                  width={44}
+                  height={31}
+                  src="https://tfcszo.vteximg.com.br/arquivos/logo-linha-do-tem.png"
+                  alt="linha do tempo"
+                />
+                <img
+                  width={48}
+                  height={48}
+                  src="https://tfcszo.vteximg.com.br/arquivos/logo-novo-inst-sobre.png"
+                  alt="linha do tempo"
+                />
+              </div>
+              <h5 class="text-[#E4A886] text-2xl text-center font-medium mb-[5px]">
+                2022
+              </h5>
+              <p class="font-normal leading-6 text-center text-[13px]">
+                Lançamento do Marketplace. Espalhar o nosso propósito e formar
+                milhares de alunos no mundo todo
+              </p>
+            </div>
+          </div>
+
+          <a
+            class="text-white border w-[294px] text-[13px] font-medium text-center block mx-auto my-0 px-0 py-3.5 rounded-[3px] border-solid border-[#171413] bg-[#171413] hover:text-[#171413] hover:border-[#171413] hover:bg-white transition-all"
+            href="https://www.maximustecidos.com.br/tecidos"
+          >
+            Veja nossos produtos
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
