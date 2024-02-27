877bfbeb1200648c3c43733e6133eb8734d0be03
regiscuba2b <reginaldo.cuba@totvs.com.br>
2024-02-27 17:56:39 -0300

    fix: yourviews piority in head

--- components/ui/YourViewsIntegration.tsx
+++ components/ui/YourViewsIntegration.tsx
@@ -1,13 +1,17 @@
+import { Head } from "$fresh/runtime.ts";
+
 function YourViewsIntegration() {
   return (
     <>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" />
-      <script
-        id="_yvsrc"
-        async
-        src="//service.yourviews.com.br/script/ae97f62a-00ed-4eb7-8fbf-024f90f5ff8a/yvapi.js"
-      >
-      </script>       
+      <Head>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" />
+        <script
+          id="_yvsrc"
+          async
+          src="//service.yourviews.com.br/script/ae97f62a-00ed-4eb7-8fbf-024f90f5ff8a/yvapi.js"
+        >
+        </script>
+      </Head>
     </>
   );
 }
