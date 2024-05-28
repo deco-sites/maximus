1a5d80454e0a34067d0ae6d307e505f0d6b7453c
Claydson Fernando <claydson.silva@totvs.com.br>
2024-05-28 15:11:09 -0300

    feat: add script rd station

--- components/ui/YourViewsIntegration.tsx
+++ components/ui/YourViewsIntegration.tsx
@@ -5,11 +5,18 @@ function YourViewsIntegration() {
     <>
       <Head>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" />
+        
         <script
           id="_yvsrc"
           async
           src="//service.yourviews.com.br/script/ae97f62a-00ed-4eb7-8fbf-024f90f5ff8a/yvapi.js"
         >
+        </script>
+
+        <script 
+          type="text/javascript" 
+          async 
+          src="//d335luupugsy2.cloudfront.net/js/loader-scripts/ddba0c19-400d-4ae3-8b26-69cdfa4c9d5c-loader.js">
         </script>
       </Head>
     </>
